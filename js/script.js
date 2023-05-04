d17e63a0201e59af99913b04e51f508dfcc0ebeb
emaile <sanya.volkov344.volkov@mail.ru>
2023-05-05 01:47:49 +0400

    1.slider

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.frontbyte__slider').slick({
+    arrows:false, //стрелки
+    adaptiveHeight:true,
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade:true,
+  });
