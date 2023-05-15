7d3867815f88a10ac1161e7ddfb181059cd97b5b
emaile <sanya.volkov344.volkov@mail.ru>
2023-05-15 22:40:27 +0400

    20450.contacts.messengers

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 const header = document.querySelector(".header")
 const scrolL = document.querySelector(".header__nav-items")
-const itemLink = document.querySelector("header__nav-item-link")
+const itemLink = document.querySelector(".header__nav-item-link")
 window.onscroll = function (event) {
   if (window.scrollY > 100) {
     header.classList.add("header__fixed")
