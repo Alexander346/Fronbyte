a55ee313efe270f0af322ebe83a35d8dcfae0171
emaile <sanya.volkov344.volkov@mail.ru>
2023-05-16 15:04:53 +0400

    20450.style changes

--- js/scroll.js
+++ js/scroll.js
@@ -3,13 +3,13 @@ const scrolL = document.querySelector(".header__nav-items")
 const itemLink = document.querySelector(".header__nav-item-link")
 const langScroll = document.querySelector(".lang")
 window.onscroll = function (event) {
-  if (window.scrollY > 100) {
+  if (scrollY > 150) {
     langScroll.classList.add("lang__scroll")
     header.classList.add("header__fixed")
     scrolL.classList.add("nav-items-scroll")
     itemLink.classList.add("nav-item-link")
 
-  } else if (window.scrollY < 100) {
+  } else if (scrollY < 100) {
     langScroll.classList.remove("lang__scroll")
     header.classList.remove("header__fixed")
     scrolL.classList.remove("nav-items-scroll")
