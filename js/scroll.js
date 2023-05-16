a40f78608497c530b4abce1e6ca1ad7c87d7c7c7
emaile <sanya.volkov344.volkov@mail.ru>
2023-05-16 11:34:52 +0400

    20450.2.header__fixed

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,16 @@
 const header = document.querySelector(".header")
 const scrolL = document.querySelector(".header__nav-items")
 const itemLink = document.querySelector(".header__nav-item-link")
+const langScroll = document.querySelector(".lang")
 window.onscroll = function (event) {
   if (window.scrollY > 100) {
+    langScroll.classList.add("lang__scroll")
     header.classList.add("header__fixed")
     scrolL.classList.add("nav-items-scroll")
     itemLink.classList.add("nav-item-link")
 
   } else if (window.scrollY < 100) {
+    langScroll.classList.remove("lang__scroll")
     header.classList.remove("header__fixed")
     scrolL.classList.remove("nav-items-scroll")
     itemLink.classList.remove("nav-item-link")
